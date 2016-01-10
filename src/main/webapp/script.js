f9913c59b7b652357a185bb6a3241ae627613985
aradej006 <aradej006@gmail.com>
2016-01-10 21:28:33 +0100

    All done

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,4 +1,4 @@
-angular.module('snmp', [])
+angular.module('snmp', ['ngAnimate', 'ui.bootstrap'])
     .controller('snmpCtrl', function ($scope, $http) {
 
         $scope.getOid = function (oid) {
@@ -8,6 +8,14 @@ angular.module('snmp', [])
             }).error(function (error) {
                 console.log(error);
             });
+        };
+
+        $scope.getValue = function(oid){
+            return $http.get('rest/snmp/getVariable/' + oid);
+        };
+
+        $scope.getTable2 = function(oid){
+            return $http.get('rest/snmp/getTable/' + oid);
         };
 
         $scope.getTable = function (oid) {
@@ -28,6 +36,89 @@ angular.module('snmp', [])
                 console.log(error);
             });
         };
+
+        $scope.getValues = function(){
+            $scope.getValue("1.3.6.1.2.1.1.3.0").success(function(result){
+                $scope.sysUpTime = result[0];
+            });
+            $scope.getValue("1.3.6.1.2.1.1.5.0").success(function(result){
+                $scope.sysName = result[0];
+            });
+            $scope.getValue("1.3.6.1.2.1.1.1.0").success(function(result){
+                $scope.systemDate = result[0];
+            });
+
+            $scope.getTable2("1.3.6.1.2.1.4.20").success(function(result){
+                $scope.ipElements = [];
+                $scope.ipHeaders = [{value:"ipAdEntAddr"},
+                    {value:"ipAdEntIfIndex"},
+                    {value:"ipAdEntNetMask"},
+                    {value:"ipAdEntBcastAddr"},
+                    {value:"ipAdEntReasmMaxSize"}];
+                for(var i = 0;i<result.length;i++){
+                    $scope.ipElements.push([]);
+                    for(var j = 0;j<result[i].length;j++){
+                        $scope.ipElements[i].push({value:result[i][j]});
+                    }
+                }
+            });
+            $scope.getTable2("1.3.6.1.2.1.6.13").success(function(result){
+                $scope.tcpElements = [];
+                $scope.tcpHeaders = [{value:"tcpConnState"},
+                    {value:"tcpConnLocalAddress"},
+                    {value:"tcpConnLocalPort"},
+                    {value:"tcpConnRemAddress"},
+                    {value:"tcpConnRemPort"}];
+                for(var i = 0;i<result.length;i++){
+                    $scope.tcpElements.push([]);
+                    for(var j = 0;j<result[i].length;j++){
+                        $scope.tcpElements[i].push({value:result[i][j]});
+                    }
+                }
+            });
+            $scope.getTable2("1.3.6.1.2.1.7.5").success(function(result){
+                $scope.udpElements = [];
+                $scope.udpHeaders = [{value:"udpLocalAddress"},
+                    {value:"udpLocalPort"}];
+                for(var i = 0;i<result.length;i++){
+                    $scope.udpElements.push([]);
+                    for(var j = 0;j<result[i].length;j++){
+                        $scope.udpElements[i].push({value:result[i][j]});
+                    }
+                }
+            });
+            $scope.getTable2("1.3.6.1.2.1.25.2.3").success(function(result){
+                $scope.hrStorageElements = [];
+                $scope.hrStorageHeaders = [{value:"hrStorageIndex"},
+                    {value:"hrStorageType"},
+                    {value:"hrStorageDescr"},
+                    {value:"hrStorageAllocationUnit"},
+                    {value:"hrStorageSize"},
+                    {value:"hrStorageUsed"},
+                    {value:"hrStorageAllocationFaliures"}];
+                for(var i = 0;i<result.length;i++){
+                    $scope.hrStorageElements.push([]);
+                    for(var j = 0;j<result[i].length;j++){
+                        $scope.hrStorageElements[i].push({value:result[i][j]});
+                    }
+                }
+            });
+            $scope.getTable2("1.3.6.1.2.1.25.3.6").success(function(result){
+                $scope.hrDiskElements = [];
+                $scope.hrDiskHeaders = [{value:"hrDiskStorageAccess"},
+                    {value:"hrDiskStorageMedia"},
+                    {value:"hrDiskStorageRemoveble"},
+                    {value:"hrDiskStorageCapacity"}];
+                for(var i = 0;i<result.length;i++){
+                    $scope.hrDiskElements.push([]);
+                    for(var j = 0;j<result[i].length;j++){
+                        $scope.hrDiskElements[i].push({value:result[i][j]});
+                    }
+                }
+            });
+        };
+
+        $scope.getValues();
 
 
     });
